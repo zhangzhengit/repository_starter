3ac5eb7f24ad79acf95054b842cd3dd37abea8b3
zhangzhen <1071324756@qq.com>
2026-05-03 10:31:00 +0800

    去除hutool中的类

--- repository_starter/src/main/java/com/vo/spring/starter/ZRepositoryStarter.java
+++ repository_starter/src/main/java/com/vo/spring/starter/ZRepositoryStarter.java
@@ -18,19 +18,15 @@ import com.vo.DMLEnum;
 import com.vo.ScanPackage;
 import com.vo.SqlResult;
 import com.vo.ZEntityHandlerScanner;
-import com.vo.ZID;
 import com.vo.ZQuery;
 import com.vo.ZRSqlMap;
 import com.vo.ZRepository;
-import com.vo.anno.ZEntity;
-import com.vo.common.STU;
+import com.vo.cache.CU;
 import com.vo.conn.Env;
 import com.vo.conn.EnvEnum;
 import com.vo.conn.ZRepositoryMain;
 import com.vo.core.ZClass;
 import com.vo.core.ZLog2;
-
-import cn.hutool.core.collection.CollUtil;
 
 /**
  *
@@ -44,14 +40,14 @@ import cn.hutool.core.collection.CollUtil;
 public class ZRepositoryStarter implements InstantiationAwareBeanPostProcessor {
 
 	private static final ZLog2 LOG = ZLog2.getInstance();
-	
+
 
 	private final AtomicBoolean gZRepository = new AtomicBoolean(false);
 
 	@Value(value = "${zrepository.scanPackageName}")
 	private Set<String> scanPackageName;
-	
-	
+
+
 	@Value(value = "${repository.actuator.enable:false}")
 	private boolean actuatorEnable;
 
@@ -59,11 +55,11 @@ public class ZRepositoryStarter implements InstantiationAwareBeanPostProcessor {
 	public boolean postProcessAfterInstantiation(final Object bean, final String beanName) throws BeansException {
 
 		Env.ENV = EnvEnum.SPRING;
-		Env.ACTUATOR_ENABLE = actuatorEnable;
-		
+		Env.ACTUATOR_ENABLE = this.actuatorEnable;
+
 		if (!this.gZRepository.get()) {
 
-			if (CollUtil.isEmpty(this.scanPackageName)) {
+			if (CU.isEmpty(this.scanPackageName)) {
 				throw new IllegalArgumentException("zrepository.scanPackageName 未配置！");
 			}
 
@@ -111,7 +107,7 @@ public class ZRepositoryStarter implements InstantiationAwareBeanPostProcessor {
 		LOG.info("ZRepositoryStarter开始扫描[{}]的子接口", ZRepository.class.getCanonicalName());
 		// 1 查找ZRepository的子接口
 		final Set<Class<?>> zrSubinterfaceSet = ZRepositoryMain.scanZRepositorySubinterface(packageName);
-		if (CollUtil.isEmpty(zrSubinterfaceSet)) {
+		if (CU.isEmpty(zrSubinterfaceSet)) {
 			LOG.info("ZRepositoryStarter没有[{}]的子接口", ZRepository.class.getCanonicalName());
 			return Collections.emptyMap();
 		}
